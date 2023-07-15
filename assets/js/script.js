955e440c6f8dc600b0738bed3a678bc6f676068c
Chris_Gruen <113554860+ChristyGruen@users.noreply.github.com>
2023-07-15 18:59:57 -0500

    icon works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,53 @@
+// //able to console log the apiKey value
+// console.log(apiKey)
 
-console.log(apiKey)
+
+
+let entireResponse = 'default'
+let currentTemp = 'default'
+let currentWind = 'default'
+let currentHumid = 'default'
+let currentSymbol = 'default'
+let localDate = dayjs().format('MM/DD/YYYY');
+console.log(localDate)
+let citySelected = 'Minneapolis'
+let cityLat = 'default'
+let cityLng = 'default'
+
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${citySelected}&appid=${apiKey}&units=imperial`
+console.log(apiURL)
+
+$('.cityContent').text(`${citySelected} (${localDate}) ICON`)
+
+
+fetch(apiURL)
+  .then(response => {
+    console.log(response)
+    return response.json();
+  })
+  .then(response => {
+    // console.log(response.newboard.grids[0].solution)// targeting the obj example
+    entireResponse = response
+    console.log(entireResponse)
+    currentTemp = response.main.temp
+    currentWind = response.wind.speed
+    currentHumid = response.main.humidity
+    currentSymbol = response.weather[0].icon  //doesn't work yet
+    var iconURL = "http://openweathermap.org/img/w/" + currentSymbol + ".png";
+    console.log(iconURL)
+    $('.cityContent').text(`${citySelected} (${localDate})`)
+    $('#wicon').attr('src', iconURL);
+    $('#cityTemp').text(`Temp: ${currentTemp}°F`)
+    $('#cityWind').text(`Wind: ${currentWind} MPH`)
+    $('#cityHumid').text(`Humidity: ${currentHumid}%`)
+
+    
+
+    console.log(currentTemp)
+    console.log(currentWind)
+    console.log(currentHumid)
+    console.log(currentSymbol)    //it just might work
+  });
+
+
+
