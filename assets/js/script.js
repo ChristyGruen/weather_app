1c8e9b1fae09033f0e392453c2c6399c4a2e0126
Chris_Gruen <113554860+ChristyGruen@users.noreply.github.com>
2023-07-15 20:16:57 -0500

    save progress, first forecast works

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,17 +11,22 @@ let currentSymbol = 'default'
 let localDate = dayjs().format('MM/DD/YYYY');
 console.log(localDate)
 let citySelected = 'Minneapolis'
-let cityLat = 'default'
-let cityLng = 'default'
+let cityLat = 44.98
+let cityLng = -93.2638
 
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${citySelected}&appid=${apiKey}&units=imperial`
-console.log(apiURL)
+
+
+// const api1URL = `https://api.openweathermap.org/data/2.5/weather?q=${citySelected}&appid=${apiKey}&units=imperial`
+// console.log(api1URL)
+
+const api2URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLng}&appid=${apiKey}&units=imperial`
+console.log(api2URL)
 
 
 $('.cityContent').text(`${citySelected} (${localDate})`)
 
 
-fetch(apiURL)
+fetch(api2URL)
   .then(response => {
     console.log(response)
     return response.json();
@@ -30,10 +35,16 @@ fetch(apiURL)
     // console.log(response.newboard.grids[0].solution)// targeting the obj example
     entireResponse = response
     console.log(entireResponse)
-    currentTemp = response.main.temp
-    currentWind = response.wind.speed
-    currentHumid = response.main.humidity
-    currentSymbol = response.weather[0].icon  //doesn't work yet
+    currentTemp = response.list[0].main.temp
+    console.log(currentTemp)
+    currentWind = response.list[0].wind.speed
+    console.log(currentWind)
+    currentHumid = response.list[0].main.humidity
+    console.log(currentHumid)
+    currentSymbol = response.list[0].weather[0].icon  //worked to get url 4 icon
+    console.log(currentSymbol)    //it just might work
+    // currentSymbol = response.weather[0].icon  //worked to get the url 4 the icon
+
     var iconURL = "http://openweathermap.org/img/w/" + currentSymbol + ".png";
     console.log(iconURL)
    
@@ -44,12 +55,73 @@ fetch(apiURL)
     $('#cityWind').text(`Wind: ${currentWind} MPH`)
     $('#cityHumid').text(`Humidity: ${currentHumid}%`)
 
-    
 
-    console.log(currentTemp)
-    console.log(currentWind)
-    console.log(currentHumid)
-    console.log(currentSymbol)    //it just might work
+
+    //first forecastDay
+    forecastDate = response.list[8].dt_txt.split(" ")[0]
+    forecastTemp = response.list[8].main.temp
+    console.log(forecastTemp)
+    forecastWind = response.list[8].wind.speed
+    console.log(forecastWind)
+    forecastHumid = response.list[8].main.humidity
+    console.log(forecastHumid)
+    forecastSymbol = response.list[8].weather[0].icon  //worked to get url 4 icon
+    console.log(forecastSymbol)    //it just might work
+    // currentSymbol = response.weather[0].icon  //worked to get the url 4 the icon
+
+    var iconURL = "http://openweathermap.org/img/w/" + forecastSymbol + ".png";
+    console.log(iconURL)
+   
+    $('.fDate1').text(`${forecastDate}`)
+    $('#fwicon1').attr('src', iconURL);
+    $('#fTemp1').text(`Temp: ${currentTemp}°F`)
+    $('#fWind1').text(`Wind: ${currentWind} MPH`)
+    $('#fHumid1').text(`Humidity: ${currentHumid}%`)
+
+
+        //first forecastDay
+    forecastDate = response.list[8].dt_txt.split(" ")[0]
+    forecastTemp = response.list[8].main.temp
+    console.log(forecastTemp)
+    forecastWind = response.list[8].wind.speed
+    console.log(forecastWind)
+    forecastHumid = response.list[8].main.humidity
+    console.log(forecastHumid)
+    forecastSymbol = response.list[8].weather[0].icon  //worked to get url 4 icon
+    console.log(forecastSymbol)    //it just might work
+    // currentSymbol = response.weather[0].icon  //worked to get the url 4 the icon
+
+    var iconURL = "http://openweathermap.org/img/w/" + forecastSymbol + ".png";
+    console.log(iconURL)
+   
+    $('.fDate1').text(`${forecastDate}`)
+    $('#fwicon1').attr('src', iconURL);
+    $('#fTemp1').text(`Temp: ${currentTemp}°F`)
+    $('#fWind1').text(`Wind: ${currentWind} MPH`)
+    $('#fHumid1').text(`Humidity: ${currentHumid}%`)
+////////////////////////////////////////////////////////////
+//try for forecast loop
+    //first forecastDay
+    forecastDate = response.list[8].dt_txt.split(" ")[0]
+    forecastTemp = response.list[8].main.temp
+    console.log(forecastTemp)
+    forecastWind = response.list[8].wind.speed
+    console.log(forecastWind)
+    forecastHumid = response.list[8].main.humidity
+    console.log(forecastHumid)
+    forecastSymbol = response.list[8].weather[0].icon  //worked to get url 4 icon
+    console.log(forecastSymbol)    //it just might work
+    // currentSymbol = response.weather[0].icon  //worked to get the url 4 the icon
+
+    var iconURL = "http://openweathermap.org/img/w/" + forecastSymbol + ".png";
+    console.log(iconURL)
+   
+    $('.fDate1').text(`${forecastDate}`)
+    $('#fwicon1').attr('src', iconURL);
+    $('#fTemp1').text(`Temp: ${currentTemp}°F`)
+    $('#fWind1').text(`Wind: ${currentWind} MPH`)
+    $('#fHumid1').text(`Humidity: ${currentHumid}%`)
+
   });
 
 
